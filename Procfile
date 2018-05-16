f1c43614b3d251c99884344eea682389095054b0
Roland Heintze <rheintze@linux.com>
2018-05-16 15:54:19 -0400

    Added Procfile & wsgi for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hibp_secureworks.wsgi
---
+++ wsgi.py
@@ -0,0 +1,9 @@
+import os
+import app
+
+
+os.environ.setdefault("LC_ALL", "en_US.UTF-8")
+os.environ.setdefault("LANG", "en_US.UTF-8")
+
+if __name__ == "__main__":
+    app.run()
